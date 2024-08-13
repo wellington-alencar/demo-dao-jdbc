d58d0171d2f9bce842c1267bea6dc2af221cf89a
Wellington Alencar <wellington.a.barreto@hotmail.com>
2024-08-13 15:34:32 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,2 +1,13 @@
-package model.dao;public interface DepartmentDao {
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
 }
